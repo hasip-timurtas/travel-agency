6086efd2d553e3d9d95bffb176e26361027658dd
Hasip Timurtaş <hasip.timurtas@gmail.com>
2021-01-02 17:56:55 +0000

    deploy sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-npm run build
-cd dist
-zip -r dist.zip .
-#zip -r build/build.zip build/.
-unzip -o dist.zip -d /home/hasip/domains/agency.hasip.dev/public_html/
+yarn build
+scp -r ./dist/* root@95.179.168.245:/home/hasip/domains/agency.hasip.dev/public_html/
